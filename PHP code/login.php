5909654e6bf26b13de362fd152d541abfb01db97
Dawidoww <b00134926@mytudublin.ie>
2022-03-25 11:22:52 +0000

    upd

--- PHP code/login.php
+++ PHP code/login.php
@@ -1,4 +1,4 @@
-<?php require_once('../template/header.php'); ?>
+<?php require_once('../templates/header.php'); ?>
 <link rel="stylesheet" type="text/css" href="../css/signin.css">
     <title>Sign in</title>
 </head>
