c4a50e6eadb6f75bb08edf5aef61b156c6b9d94d
B00138946 <B00138946@mytudublin.ie>
2022-04-15 16:13:06 +0100

    conf

---
+++ PHP code/confirmation.php
@@ -0,0 +1,44 @@
+<?php require_once('../templates/header.php'); ?>
+<link rel="stylesheet" href="../css/confirmation.css"/>
+
+<body>
+<div class="page">
+    <div class="box1">
+        <h1>Order Confirmation</h1>
+        <br>
+        <p>Hi Disi Pepiq.</p>
+        <p>Thank you for ordering via critical Fitness.</p>
+        <p>A record of purchase information appears below.</p>
+        <br>
+
+        <h2>Order Information</h2>
+        <br>
+        <p>Order Date: 15/04/2022</p>
+        <p>Order ID: </p>
+<br>
+        <h2>Item</h2>
+        <br>
+        <p>Plan 1</p>
+<br>
+        <h2>.</h2>
+        <br>
+        <p class = "p1" >Order Amount: </p>
+        <p class = "p1">Promo code: </p>
+        <p class = "p1">Order Total: </p>
+        <br>
+        <p class = "p1">If you have a question about this order, you can simply email us at admin@critcalfitness.com with your question and we will get back to you shortly with an answer</p>
+
+
+
+        <a class ="a2" href="test1.php"><button1 name="Back" value="BackB" class="button" type="Submit">Back</button1></a>
+    </div>
+
+
+
+
+</div>
+
+</body>
+
+
+<?php require_once('../templates/footer.php'); ?>
