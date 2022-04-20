4f16624c70502ef5c3d4a2cae07ef7f78e919002
B00138946 <B00138946@mytudublin.ie>
2022-04-20 17:39:10 +0100

    neww

--- PHP code/confirmation.php
+++ PHP code/confirmation.php
@@ -6,15 +6,15 @@
     <div class="box1">
         <h1>Order Confirmation</h1>
         <br>
-        <p>Hi Disi Pepiq.</p>
+        <p>Hi, </p>
         <p>Thank you for ordering via critical Fitness.</p>
         <p>A record of purchase information appears below.</p>
         <br>
 
         <h2>Order Information</h2>
         <br>
-        <p>Order Date: 15/04/2022</p>
-        <p>Order ID: </p>
+        <p class = "p1" >Order Date:</p>
+        <p class = "p1" >Order ID:</p>
 <br>
         <h2>Item</h2>
         <br>
@@ -26,7 +26,7 @@
         <p class = "p1">Promo code: </p>
         <p class = "p1">Order Total: </p>
         <br>
-        <p class = "p1">If you have a question about this order, you can simply email us at admin@critcalfitness.com with your question and we will get back to you shortly with an answer</p>
+        <p> If you have a question about this order, you can simply email us at admin@critcalfitness.com with your question and we will get back to you shortly with an answer</p>
 
 
         <a class ="a2" href="test1.php"><button1 name="Back" value="BackB" class="button" type="Submit">Back</button1></a>
--- PHP code/test1.php
+++ PHP code/test1.php
@@ -4,18 +4,29 @@
     <div class="box1">
         <h2> Status: You are logged in <?php echo $_SESSION['email'];?></h2>
         <form action="logout.php" method="post" name="Logout" class="form-signin">
+            <br>
             <button name="Submit" value="Logout" class="button" type="submit">Log out</button>
         </form>
     </div>
 
     <div class="box2">
         <h1>About us</h1>
-        <p>Ut eu dolor malesuada, egestas felis id, accumsan sapien. Vestibulum aliquet scelerisque viverra. Donec id lectus enim. Nullam consectetur feugiat lectus, vel rhoncus lorem pretium id. Integer eleifend, ante a pharetra consequat, sem nunc malesuada sapien, quis scelerisque orci elit non lacus. Donec euismod, lectus et elementum fermentum, metus sapien iaculis odio, in scelerisque leo ex eget felis. Nunc massa massa, feugiat congue lacus a, laoreet faucibus nunc. Ut faucibus, neque in finibus imperdiet, felis justo maximus quam, a scelerisque lectus urna sed lacus. Aenean pretium vitae sapien a vulputate. Morbi tortor quam, dictum sit amet quam id, finibus lacinia velit. Mauris dapibus fringilla nibh.Ut eu dolor malesuada, egestas felis id, accumsan sapien. Vestibulum aliquet scelerisque viverra.Donec id lectus enim. Nullam consectetur feugiat lectus </p>
+<p> Established in 2022. We Provide customers  with a
+    fitness plan that will help them reach any fitness goals they would like to achieve,
+    that being losing weight, gaining weight, gaining strength, or just getting into shape,
+    Critical Fitness website will be able to provide that. This website is suitable for all ages groups.</p>
+    <br>
+        <br>
+        <br>
+        <h2>Buy a plan just with 5 clicks</h2>
+        <br>
+        <h2>10% discount for Students</h2>
     </div>
 
     <div class="box3">
-        <h1>Flex item </h1>
-        <p>Ut eu dolor malesuada, egestas felis id, accumsan sapien. Vestibulum aliquet scelerisque viverra. Donec id lectus enim. Nullam consectetur feugiat lectus, vel rhoncus lorem pretium id. Integer eleifend, ante a pharetra consequat, sem nunc malesuada sapien, quis scelerisque orci elit non lacus. Donec euismod, lectus et elementum fermentum, metus sapien iaculis odio, in scelerisque leo ex eget felis. Nunc massa massa, feugiat congue lacus a, laoreet faucibus nunc. Ut faucibus, neque in finibus imperdiet, felis justo maximus quam, a scelerisque lectus urna sed lacus. Aenean pretium vitae sapien a vulputate. Morbi tortor quam, dictum sit amet quam id, finibus lacinia velit. Mauris dapibus fringilla nibh.Ut eu dolor malesuada, egestas felis id, accumsan sapien. Vestibulum aliquet scelerisque viverra. Donec id lectus enim. Nullam consectetur feugiat lectus, vel rhoncus lorem pretium id. Integer eleifend, ante a pharetra consequat, sem nunc malesuada sapien, quis scelerisque orci elit non lacus. Donec euismod, lectus et elementum fermentum, metus sapien iaculis odio, in scelerisque leo ex eget felis. Nunc massa massa, feugiat congue lacus a, laoreet faucibus nunc.</p>
+        <h1>Workout of the Day</h1>
+        <br>
+        <img id = workout src="../img/workout.jpg" alt=work" />
     </div>
 </div>
 <?php require_once('../templates/footer.php'); ?>
