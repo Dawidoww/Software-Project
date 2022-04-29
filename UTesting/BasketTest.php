b04d1311950533a1438721da1ebad397f9bf23ea
VadimProcik <b00132224@mytudublin.ie>
2022-04-29 09:24:37 +0100

    PHPUnit&UnitTesting
    PHP Unit download and testing

---
+++ UTesting/BasketTest.php
@@ -0,0 +1,54 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class BasketTest extends TestCase
+{
+
+    public function testGetPlanName()
+    {
+
+    $this -> assertSame($this->testGetPlanName());
+    }
+
+    public function testAddItem()
+    {
+    $this -> assertSame($this->testAddItem());
+    }
+
+    public function testSetPlanName()
+    {
+
+    }
+
+    public function testRemoveItem()
+    {
+
+    }
+
+    public function testCalcTotal()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGetAmount()
+    {
+
+    }
+
+    public function testSetAmount()
+    {
+
+    }
+
+    public function testCheckOut()
+    {
+
+    }
+}
